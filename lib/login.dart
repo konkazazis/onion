ab0013aeabda71a8fc3c0d6798dddf4178f8f6f3
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2025-03-26 20:52:17 +0100

    fix

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'signup.dart';
-//import 'package:auth_firebase/services/auth_service.dart';
+import 'services/auth_service.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
