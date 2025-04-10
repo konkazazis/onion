f4a33ba619a9163b715596f3d76dd019ae7e98ea
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2025-04-10 18:54:04 +0200

    profile updates

--- lib/profile.dart
+++ lib/profile.dart
@@ -6,12 +6,6 @@ class ProfileComponent extends StatelessWidget {
   final String name;
   final String email;
   final String profileImageUrl;
-  static const List<String> profileOptions = [
-    'Personal Information',
-    'Notification Settings',
-    'Work Details',
-    'Change Password'
-  ];
 
   const ProfileComponent({
     Key? key,
@@ -35,46 +29,100 @@ class ProfileComponent extends StatelessWidget {
               backgroundImage: NetworkImage(profileImageUrl),
             ),
             const SizedBox(height: 12),
-            Text(name,
-                style:
-                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            Text(email,
-                style: const TextStyle(fontSize: 16, color: Colors.grey)),
-
+            Text(
+              name,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              email,
+              style: const TextStyle(fontSize: 16, color: Colors.grey),
+            ),
             const SizedBox(height: 24),
             const Divider(),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: profileOptions.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.white,
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    side: const BorderSide(color: Colors.grey, width: 1),
-                  ),
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(8),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => PersonalDetails()),
-                      );
-                      print("Tapped on ${profileOptions[index]}");
-                    },
-                    child: ListTile(
-                      leading: const Icon(Icons.event),
-                      title: Text(profileOptions[index]),
-                    ),
-                  ),
-                );
-              },
+            Card(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+                side: const BorderSide(color: Colors.grey, width: 1),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PersonalDetails()),
+                  );
+                },
+                child: const ListTile(
+                  leading: Icon(Icons.event),
+                  title: Text('Personal Details'),
+                ),
+              ),
             ),
-
-            const SizedBox(height: 32),
+            Card(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+                side: const BorderSide(color: Colors.grey, width: 1),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PersonalDetails()),
+                  );
+                },
+                child: const ListTile(
+                  leading: Icon(Icons.event),
+                  title: Text('Notification Settings'),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+                side: const BorderSide(color: Colors.grey, width: 1),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PersonalDetails()),
+                  );
+                },
+                child: const ListTile(
+                  leading: Icon(Icons.event),
+                  title: Text('Work Details'),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+                side: const BorderSide(color: Colors.grey, width: 1),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(8),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PersonalDetails()),
+                  );
+                },
+                child: const ListTile(
+                  leading: Icon(Icons.event),
+                  title: Text('Change Password'),
+                ),
+              ),
+            ),
             _signout(context),
           ],
         ),
