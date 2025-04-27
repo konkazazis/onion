2b80f34612ca27f46e36e254f0ba3429d3969711
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2025-04-27 12:10:44 +0200

    fixes

--- lib/shift_edit.dart
+++ lib/shift_edit.dart
@@ -1,16 +1,13 @@
-import 'dart:developer';
-import 'package:flutter/services.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'services/details_service.dart';
 
 class ShiftEdit extends StatefulWidget {
-  final Map<String, dynamic> shift;
   final String userID;
   final String email;
+  final Map<String,dynamic> shift;
   const ShiftEdit({super.key, required this.userID, required this.email, required this.shift});
 
   @override
@@ -18,197 +15,248 @@ class ShiftEdit extends StatefulWidget {
 }
 
 class _ShiftEditState extends State<ShiftEdit> {
-
-  TextEditingController companyController = TextEditingController();
-  TextEditingController positionController = TextEditingController();
-  TextEditingController timeController = TextEditingController();
-  TextEditingController locationController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController perHourController = TextEditingController();
-  var readOnly = true;
-  var company = '';
-  var position = '';
+  DateTime? selectedDate;
+  TimeOfDay? startTime;
+  TimeOfDay? endTime;
+  String? selectedWorkType;
+  String notes = "";
+  var selectedHour = 0;
   var brakeTime = '';
-  var email = '';
-  var location = '';
-  var perHour = '';
+  var overtime = '';
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = DateTime.parse(widget.shift['date']);
+  }
+
+  final List<String> workTypes = [
+    'Morning',
+    'Afternoon',
+    'Night',
+    'Remote',
+    'On-site'
+  ];
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() => selectedDate = picked);
+    }
+  }
+
+  String formatTimeOfDay(TimeOfDay time) {
+    return '${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}';
+  }
+
+  Future<void> _selectTime(BuildContext context, bool isStartTime) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: isStartTime
+          ? (startTime ?? TimeOfDay.now())
+          : (endTime ?? TimeOfDay.now()),
+    );
+    if (picked != null) {
+      setState(() => isStartTime ? startTime = picked : endTime = picked);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(child: Text(
-                    'Personal Details',
-                    style: GoogleFonts.raleway(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+              crossAxisAlignment:
+              CrossAxisAlignment.center, // Center horizontally
+              mainAxisSize:
+              MainAxisSize.min, // Prevent Column from taking full heigh
+              children: [
+                Text(
+                  'This is where you plan your next shift',
+                  style: GoogleFonts.raleway(
                       textStyle: const TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28)),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(
+                              color: Colors.black, width: 1), // Optional border
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            side: const BorderSide(color: Colors.grey, width: 1),
+                          ),
+                        ),
+                        onPressed: () => _selectDate(context),
+                        child: Text(
+                          style: TextStyle(color: Colors.black),
+                          selectedDate != null
+                              ? 'Date: ${DateFormat('yyyy-MM-dd').format(selectedDate!)}'
+                              : 'Select Date',
+                        ),
                       ),
                     ),
-                  ),),
-                  Expanded(child: SwitchListTile(
-                    value: !readOnly,
-                    onChanged: (bool value) {
-                      setState(() {
-                        readOnly = !readOnly;
-                      });
-                    },
-                  ),)
-                ],
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                enabled: !readOnly,
-                readOnly: readOnly,
-                controller: companyController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    labelText: 'Company'
+                  ],
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    company = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                enabled: !readOnly,
-                readOnly: readOnly,
-                controller: positionController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    labelText: 'Position'
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    position = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                enabled: !readOnly,
-                readOnly: readOnly,
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ],
-                controller: timeController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    labelText: 'Brake time (minutes)'
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    brakeTime = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                enabled: !readOnly,
-                readOnly: readOnly,
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ],
-                controller: perHourController,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Money per hour (nett)'
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    perHour = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                enabled: !readOnly,
-                readOnly: readOnly,
-                controller: emailController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    labelText: 'Email'
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                enabled: !readOnly,
-                readOnly: readOnly,
-                controller: locationController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                    labelText: 'Location'
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    location = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 16),
-              Center(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    side: readOnly ? BorderSide(color: Colors.grey, width: 1) : BorderSide(color: Colors.black, width: 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(
+                              color: Colors.black, width: 1), // Optional border
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            side: const BorderSide(color: Colors.grey, width: 1),
+                          ),
+                        ),
+                        onPressed: () => _selectTime(context, true),
+                        child: Text(
+                          style: TextStyle(color: Colors.black),
+                          startTime != null
+                              ? 'Start: ${startTime!.format(context)}'
+                              : 'Select Start Time',
+                        ),
+                      ),
                     ),
-                  ),
-                  onPressed: readOnly ? null : null,
-                  child: Text(
-                    style: readOnly ? TextStyle(color: Colors.grey) : TextStyle(color: Colors.black),
-                    'Save',
-                  ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          side: BorderSide(
+                              color: Colors.black, width: 1), // Optional border
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            side: const BorderSide(color: Colors.grey, width: 1),
+                          ),
+                        ),
+                        onPressed: () => _selectTime(context, false),
+                        child: Text(
+                          style: TextStyle(color: Colors.black),
+                          endTime != null
+                              ? 'End: ${endTime!.format(context)}'
+                              : 'Select End Time',
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 16),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 24.0),
+                const SizedBox(height: 16),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  //controller: timeController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Overtime (minutes)'
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      overtime = value;
+                      print(overtime);
+                    });
+                  },
+                ),
+                const SizedBox(width: 16),
+                const SizedBox(height: 16),
+                DropdownButtonFormField<String>(
+                  isExpanded: true,
+                  dropdownColor: Colors.white,
+                  focusColor: Colors.black,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius:
+                      BorderRadius.circular(8),
+                      borderSide: BorderSide(
+                          color: Colors.black, width: 1),
+                    ),
+                    contentPadding:
+                    EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  ),
+                  hint: Text("Select a work type"),
+                  value: selectedWorkType,
+                  items: workTypes
+                      .map((type) => DropdownMenuItem<String>(
+                    value: type,
+                    child: Text(type),
+                  ))
+                      .toList(),
+                  onChanged: (value) =>
+                      setState(() => selectedWorkType = value),
+                ),
+                const SizedBox(height: 24),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Notes',
+                  ),
+                  onChanged: (value) => setState(() => notes = value),
+                ),
+                const SizedBox(height: 16),
+                Center(
                   child: ElevatedButton(
-                    onPressed: () => Navigator.pop(context),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
-                      foregroundColor: Colors.black,
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(15.0),
-                      elevation: 6.0,
-                      shadowColor: Colors.black54,
+                      side: BorderSide(color: Colors.black, width: 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        side: const BorderSide(color: Colors.grey, width: 1),
+                      ),
                     ),
-                    child: const Icon(
-                      Icons.close,
-                      size: 25.0,
+                    onPressed: () async {
+                      Navigator.pop(context, 'refresh');
+                    },
+                    child: const Text(
+                        style: TextStyle(color: Colors.black), 'Save Shift'),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        bottom: 24.0), // Adjust padding as needed
+                    child: ElevatedButton(
+                      onPressed: () => Navigator.pop(context, 'refresh'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: const CircleBorder(), // Fully round button
+                        padding: const EdgeInsets.all(
+                            15.0), // Increase for a bigger button
+                        elevation: 6.0,
+                        shadowColor: Colors.black54,
+                      ),
+                      child: const Icon(
+                        Icons.close,
+                        size: 25.0, // Bigger icon
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
+              ],
+            ),
+          )),
     );
   }
 }
